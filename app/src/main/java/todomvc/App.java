364a0e53626709955dd6be63e4b9fea43fb2d17b
S-Spencer <stefanspencer@hotmail.co.uk>
2023-05-17 10:48:19 +0100

    Testing access

--- app/src/main/java/todomvc/App.java
+++ app/src/main/java/todomvc/App.java
@@ -5,7 +5,7 @@ package todomvc;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello Makers!";
     }
 
     public static void main(String[] args) {
